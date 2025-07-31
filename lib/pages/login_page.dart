9ae65d8d6a84363f08d4dbeb45b82f5c199b36bb
平川華花 <hirakawahanaka@hirakawahanahananoMac-mini.local>
2025-07-31 14:30:45 +0900

    ログイン機能実装

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:naily/features/login/components/login_ui.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LoginUi();
+  }
+}
