2e9dc315a41250a8ec30d4d4284c2981b0894f91
平川華花 <hirakawahanaka@hirakawahanahananoMac-mini.local>
2025-07-31 14:28:15 +0900

    ログイン機能実装

---
+++ lib/features/login/components/login_ui.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:naily/features/login/repositories/login_signin.dart';
+import 'package:naily/pages/feed_page.dart';
+
+class LoginUi extends StatefulWidget {
+  const LoginUi({super.key});
+
+  @override
+  State<LoginUi> createState() => _LoginUiState();
+}
+
+class _LoginUiState extends State<LoginUi> {
+  String email = '';
+  String password = '';
+  bool isLogin = false;
+  String? errorMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              onChanged: (value) {
+                email = value;
+              },
+              decoration: const InputDecoration(
+                labelText: 'メールアドレス',
+              ),
+            ),
+            TextField(
+              onChanged: (value) {
+                password = value;
+              },
+              decoration: const InputDecoration(
+                labelText: 'パスワード',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                isLogin = await signInWithEmail(email, password);
+                if (isLogin) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const FeedPage()),
+                  );
+                } else {
+                  errorMessage = 'メールアドレスまたはパスワードが正しくありません';
+                }
+              },
+              child: const Text('ログイン'),
+            ),
+            if (errorMessage != null)
+              Text(
+                errorMessage!,
+                style: const TextStyle(color: Colors.red),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
