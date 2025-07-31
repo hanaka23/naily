94483918a99fdd4cdbaa4b619f264acad6bde02f
平川華花 <hirakawahanaka@hirakawahanahananoMac-mini.local>
2025-07-31 16:22:10 +0900

    実行が中断されるエラー解消

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:firebase_core/firebase_core.dart'; // 追加
 import 'package:naily/features/login/repositories/login_user_state.dart';
 import 'package:naily/pages/feed_page.dart';
 import 'package:naily/pages/login_page.dart';
-
+import 'core/datasources/firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // 追加
-  await Firebase.initializeApp(); // 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
