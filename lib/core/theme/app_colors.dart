b119bd48fb2110296a5488db140a87d45317a962
平川華花 <hirakawahanaka@hirakawahanahananoMac-mini.local>
2025-08-01 12:00:15 +0900

    theme colorコンポーネント作成

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // UIカラー
+  static const Color gray = Color(0xFFEEEEF0);
+  static const Color darkGray = Color(0xFFD9D9D9);
+  static const Color pink = Color(0xFFEF6163);
+
+  // テキストカラー
+  static const Color textGray = Color(0xFFABABAB);
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+
+  // エラーカラー
+  static const Color error = Colors.red;
+}
