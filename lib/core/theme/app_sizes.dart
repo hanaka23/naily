eda13a92f8129761157d0355db002c70c212e19a
平川華花 <hirakawahanaka@hirakawahanahananoMac-mini.local>
2025-08-01 16:29:45 +0900

    AppBarコンポーネント化

---
+++ lib/core/theme/app_sizes.dart
@@ -0,0 +1,4 @@
+class AppSizes {
+  static const double appBarHeight = 32.0;
+  static const double buttonHeight = 48.0;
+}
--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:naily/core/components/appbar.dart';
 import 'package:naily/core/theme/app_colors.dart';
 import 'package:naily/core/theme/app_spacing.dart';
 import 'package:naily/features/signup/components/signup_ui.dart';
@@ -10,10 +11,7 @@ class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(32), // ← 高さを変更
-        child: AppBar(),
-      ),
+      appBar: const MinimalAppBar(),
       body: Container(
         width: double.infinity,
         height: double.infinity,
