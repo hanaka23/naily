079bca812512c5166c55bc2d14b52210cb685b16
平川華花 <hirakawahanaka@hirakawahanahananoMac-mini.local>
2025-08-01 15:23:27 +0900

    spacingのサイズ定義作成

---
+++ lib/core/theme/app_spacing.dart
@@ -0,0 +1,9 @@
+class AppSpacing {
+  static const double xxs = 4.0;
+  static const double xs = 8.0;
+  static const double sm = 16.0;
+  static const double md = 24.0;
+  static const double lg = 32.0;
+  static const double xl = 48.0;
+  static const double xxl = 64.0;
+}
