4fb636c8d0b73845a15f65edbf8cc2e7d3ce48a9
平川華花 <hirakawahanaka@hirakawahanahananoMac-mini.local>
2025-08-07 17:28:09 +0900

    entity作成　image_pickerインストール

---
+++ lib/features/post/components/add_images_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostImages extends StatefulWidget {
+  const PostImages({super.key});
+
+  @override
+  State<PostImages> createState() => _PostImagesState();
+}
+
+class _PostImagesState extends State<PostImages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/post/components/edit_discription_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _PostDiscription extends StatefulWidget {
+  const _PostDiscription({super.key});
+
+  @override
+  State<_PostDiscription> createState() => __PostDiscriptionState();
+}
+
+class __PostDiscriptionState extends State<_PostDiscription> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
